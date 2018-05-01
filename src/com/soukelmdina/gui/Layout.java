4aaf3c8dc218b107a5fad0b0ecd71b3fbe0108c0
marzoukmariem <36210586+marzoukmariem@users.noreply.github.com>
2018-05-01 11:14:05 +0100

    test2

--- src/com/soukelmdina/gui/Layout.java
+++ src/com/soukelmdina/gui/Layout.java
@@ -8,7 +8,6 @@ package com.soukelmdina.gui;
 import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
-import com.soukelmdina.entite.Evenement;
 import com.soukelmdina.entite.Utilisateur;
 
 /**
@@ -16,15 +15,12 @@ import com.soukelmdina.entite.Utilisateur;
  * @author mosla
  */
 public class Layout {
-
-    Form f;
-    public static String name = "yassine";
-    public static String URL = "http://127.0.0.1";
-    public static Utilisateur user = null;
-
+      Form f;
+      public static String name="yassine";
+      public static String URL="http://127.0.0.1"; 
+      public static Utilisateur user = null;
     public Layout() {
         f = new Form();
-<<<<<<< HEAD
         f.getToolbar().addMaterialCommandToSideMenu("Accueil",  FontImage.MATERIAL_HOME,(e)->{HomeForm home=new HomeForm();
         home.getF().show();});
         
@@ -64,38 +60,6 @@ public class Layout {
         }}
  
     
-=======
-        f.getToolbar().addMaterialCommandToSideMenu("Accueil", FontImage.MATERIAL_HOME, (e) -> {
-            HomeForm home = new HomeForm();
-            home.getF().show();
-        });
-        if (user == null) {
-            f.getToolbar().addMaterialCommandToSideMenu("Login", FontImage.MATERIAL_HOME, (e) -> {
-                Login login = new Login();
-                login.getF().show();
-            });
-        } else {
-            f.getToolbar().addMaterialCommandToSideMenu("Profile", FontImage.MATERIAL_HOME, (e) -> {
-                Profile profile = new Profile();
-                profile.getF().show();
-            });
-            f.getToolbar().addMaterialCommandToSideMenu("Espace d'exposition", FontImage.MATERIAL_HOME, (e) -> {
-                espaceexpohome es = new espaceexpohome();
-                es.getF().show();
-            });
-            f.getToolbar().addMaterialCommandToSideMenu("Evenements", FontImage.MATERIAL_HOME, (e) -> {
-                HomeEvents ev = new HomeEvents();
-                ev.getF().show();
-            });
-            f.getToolbar().addMaterialCommandToSideMenu("Déconnexion", FontImage.MATERIAL_HOME, (e) -> {
-                user = null;
-                HomeForm home = new HomeForm();
-                home.getF().show();
-            });
-        }
-
-    }
->>>>>>> 03b8e42220cffd4968192e69b6ac34759cd6b5ca
 
     public Form getF() {
         return f;
